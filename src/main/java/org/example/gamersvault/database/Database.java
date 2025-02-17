ab01b4de066f10e43852dc048addb97076824f07
1ASHOR <timvanvliet538@gmail.com>
2025-02-17 12:10:09 +0100

    Update Database.java
    Database class filled with connection.

--- src/main/java/org/example/gamersvault/database/Database.java
+++ src/main/java/org/example/gamersvault/database/Database.java
@@ -1,4 +1,24 @@
 package org.example.gamersvault.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
+
+    private Connection conn;
+
+    public Database() {
+
+        try {
+            this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gamers_vault", "root", "");
+            System.out.println("Connected to database");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
 }
