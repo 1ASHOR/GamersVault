affe695baa9f8a44e9b1c25ff3e560e08501f9a2
1ASHOR <timvanvliet538@gmail.com>
2025-02-23 21:13:56 +0100

    GameDetailScreen changes
    Changes made to GameDetailScreen, to view selected game in detail. Code is incomplete and the function does not show asked content yet. This commit is simply to continue work on other device.

--- src/main/java/org/example/gamersvault/screens/GameDetailScreen.java
+++ src/main/java/org/example/gamersvault/screens/GameDetailScreen.java
@@ -1,22 +1,36 @@
 package org.example.gamersvault.screens;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.example.gamersvault.database.Database;
+import org.example.gamersvault.database.VaultController;
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class GameDetailScreen extends VaultScreen {
 
     private Button returnToVault;
+    private Database database;
 
     public GameDetailScreen(Stage stage) {
         super(stage);
+
         addScrollPane();
         returnToVault(stage);
     }
 
     @Override
     protected void addScrollPane(){
-        
+        super.addScrollPane();
+
+        super.getScrollPane().setContent(viewGame());
     }
 
     @Override
@@ -24,13 +38,31 @@ public class GameDetailScreen extends VaultScreen {
         super.addHBox();
         returnToVault = new Button("Return to Vault");
         super.getHBox().getChildren().add(returnToVault);
-    }
 
+    }
 
     private void returnToVault(Stage stage) {
         returnToVault.setOnAction(e -> {
             VaultScreen vaultScreen = new VaultScreen(stage);
-            stage.close();
         });
+    }
+
+    private ScrollPane viewGame(){
+        database = new Database();
+        ScrollPane scrollGame = new ScrollPane();
+        try {
+            Statement stmt = database.getConnection().createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM game WHERE name LIKE '"+ super.getVaultController().getSelectedGame() +"'");
+
+            while(rs.next()){
+                scrollGame.setContent(new Label(rs.getString("name")));
+                System.out.println(rs.getString("name"));
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return scrollGame;
     }
 }
