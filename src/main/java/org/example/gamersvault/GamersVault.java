1987e6095dcf2377b8850927ffa3ca5ee7d0663e
1ASHOR <timvanvliet538@gmail.com>
2026-02-22 17:57:30 +0100

    Update GamersVault.java

--- src/main/java/org/example/gamersvault/GamersVault.java
+++ src/main/java/org/example/gamersvault/GamersVault.java
@@ -7,16 +7,22 @@ import org.example.gamersvault.screens.VaultScreen;
 import java.io.IOException;
 
 public class GamersVault extends Application {
+    // Deze methode wordt automatisch aangeroepen bij het starten van de JavaFX-applicatie
     @Override
     public void start(Stage stage) throws IOException {
 
+        // Maak het hoofdscherm (VaultScreen) aan en geef de primaire stage mee
         VaultScreen homeScreen = new VaultScreen(stage);
 
+        // Zorg dat het venster niet handmatig vergroot/verkleind kan worden
         stage.setResizable(false);
+        // Stel de titel van het venster in
         stage.setTitle("The Gamers Vault");
+        // Toon het venster
         stage.show();
     }
 
+    // Main-methode: startpunt van het programma
     public static void main(String[] args) {
         launch();
     }
